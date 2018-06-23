6ea9f3684b58c70e96305faeda3dd2e6049a3b1f
alok <kurdleak.alok@gmail.com>
2018-06-23 07:20:11 +0530

    removed loss from cl methods. And corrected getting of maps from grad

--- GAIN.py
+++ GAIN.py
@@ -6,40 +6,43 @@ class GAIN(chainer.Chain):
 	def __init__(self):
 		super(GAIN, self).__init__()
 
-
-	def stream_cl(self, inp, cl_target, final_conv_layer, grad_target_layer='prob', class_id=None):
+	def stream_cl(self, inp, final_conv_layer, grad_target_layer='prob', class_id=None):
 		h = chainer.Variable(inp)
-		# activations = {'input': h}
-
 		for key, funcs in self.functions.items():
 			for func in funcs:
 				h = func(h)
+			if key == final_conv_layer:
+				activation = h
 			if key == grad_target_layer:
 				break
 
-		gcam = self.get_gcam(h, getattr(self, final_conv_layer), class_id)
-		loss = F.softmax_cross_entropy(h, cl_target)
-		return loss, gcam
+		gcam = self.get_gcam(h, activation, class_id)
+		return gcam, h
 
 
-	def stream_am(self, masked_image, cl_target, class_id):
-		pass
+	def stream_am(self, masked_image, class_id):
+		h = chainer.Variable(masked_image)
+		for key, funcs in self.functions.items():
+			for func in funcs:
+				h = func(h)
+
+		return h, class_id
 
 
-	def get_gcam(self, end_output, conv_link, class_id=None):
+	def get_gcam(self, end_output, activations, class_id=None):
 		self.cleargrads()
 		self.set_init_grad(end_output, class_id)
-		end_output.backward()
+		end_output.backward(retain_grad=True)
 
-		grad = conv_link.W.grad_var
+		grad = activations.grad_var
 		grad = F.average_pooling_2d(grad, grad.shape[2], 1)
 		grad = F.expand_dims(F.reshape(grad, (grad.shape[0]*grad.shape[1], grad.shape[2], grad.shape[3])), 0)
 
-		weights = conv_link.W
+		weights = activations
 		weights = F.expand_dims(F.reshape(weights, (weights.shape[0]*weights.shape[1], weights.shape[2], weights.shape[3])), 0)
 
 		self.cleargrads()
-		return F.resize_images(F.convolution_2d(weights, grad, None, 1, 0), (self.size, self.size))
+		return F.resize_images(F.relu(F.convolution_2d(weights, grad, None, 1, 0)), (self.size, self.size))
 
 
 	def set_init_grad(self, var, class_id=None):
@@ -49,10 +52,12 @@ class GAIN(chainer.Chain):
 		else:
 			var.grad[0][class_id] = 1
 
-	def mask_image(self, gcam, img, sigma, w):
-		mask = F.squeeze(F.sigmoid(-w * (gcam - sigma)))
-		for channel_depth in range(img.shape[-3]):
-			img[0][channel_depth]*mask
-		return img
+	def get_mask(self, gcam, sigma, w):
+		gcam = (gcam - F.min(gcam).data)/(F.max(gcam) - F.min(gcam)).data
+		mask = F.squeeze(F.sigmoid(w * (gcam - sigma)))
 
-
+		return mask
+	def mask_image(self, img, mask):
+		broadcasted_mask = F.broadcast_to(mask, img.shape)
+		to_subtract = img*broadcasted_mask
+		return img - to_subtract
