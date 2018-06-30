951aca70cd90c3525cb34c608287ce330598d792
alok <kurdleak.alok@gmail.com>
2018-06-30 09:32:52 +0530

    removed model as an arguement. Need to pass setup optimizer

--- updater.py
+++ updater.py
@@ -5,21 +5,22 @@ from chainer import functions as F
 from chainer.backends.cuda import get_array_module
 import numpy as np
 
+
 class VOC_ClassificationUpdater(StandardUpdater):
-	def __init__(self, iterator, optimizer, model, no_of_classes=21, device=-1):
+	def __init__(self, iterator, optimizer, no_of_classes=21, device=-1):
 		super(VOC_ClassificationUpdater, self).__init__(iterator, optimizer)
-		self.model = model
+		# self.model = model
 		self.device = device
-		if self.device >= 0:
-			self.model.to_gpu(self.device)
-		self._optimizers['main'].setup(self.model)
+		# if self.device >= 0:
+		# 	self.model.to_gpu(self.device)
+		# self._optimizers['main'].setup(self.model)
 		self.no_of_classes=no_of_classes
 	def update_core(self):
 		image, labels = self.converter(self.get_iterator('main').next())
 		image = Variable(image)
 		if self.device >= 0:
 			image.to_gpu(self.device)
-		cl_output = self.model.classify(image)
+		cl_output = self._optimizers['main'].target.classify(image)
 		xp = get_array_module(cl_output.data)
 
 		target = xp.asarray([[0]*self.no_of_classes]*cl_output.shape[0])
@@ -27,11 +28,10 @@ class VOC_ClassificationUpdater(StandardUpdater):
 			gt_labels = np.unique(labels[i]).astype(np.int32)[2:] - 1 # Not considering -1 & 0
 			target[i][gt_labels] = 1
 		loss = F.sigmoid_cross_entropy(cl_output, target, normalize=True)
-		report({'Loss':loss}, self.model)
-		self.model.cleargrads()
+		report({'Loss':loss}, self.get_optimizer('main').target)
+		self._optimizers['main'].target.cleargrads()
 		loss.backward()
 		self._optimizers['main'].update()
-
 
 
 
