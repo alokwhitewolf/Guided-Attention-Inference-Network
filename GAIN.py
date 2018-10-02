b283aa22834d539714e82de71ca2e02ab185c402
alok <kurdleak.alok@gmail.com>
2018-10-02 07:54:03 +0530

    added funcs to add GAIN stuff for generic class

--- GAIN.py
+++ GAIN.py
@@ -5,7 +5,11 @@ import chainer.functions as F
 class GAIN(chainer.Chain):
 	def __init__(self):
 		super(GAIN, self).__init__()
-		# To override in child class
+		# To override in child class or 
+		# set after initiations from respective class function
+		# eg see set_final_conv_layer, set_grad_target_layer
+		# set_GAIN_functions
+		
 		self.size = None  # Size of images
 		self.GAIN_functions = None  # Refer files in /models
 		self.final_conv_layer = None
@@ -62,11 +66,22 @@ class GAIN(chainer.Chain):
 		self.freezed_layers = links_list
 
 	def freeze_layers(self):
-
 		for link in self.freezed_layers:
 			getattr(self, link).disable_update()
 
+	def set_final_conv_layer(self, layername):
+		self.final_conv_layer = layername
 
+	def set_grad_target_layer(self, layername):
+		self.grad_target_layer = layername
+
+	def set_GAIN_functions(self, ordered_dict):
+		for key in ordered_dict.keys():
+			for item_no in range(len(ordered_dict[key])):
+				if isinstance(ordered_dict[key][item_no], str):
+					ordered_dict[key][item_no] = getattr(self, ordered_dict[key][item_no])
+		self.GAIN_functions = ordered_dict
+		
 	@staticmethod
 	def get_mask(gcam, sigma=.5, w=8):
 		gcam = (gcam - F.min(gcam).data)/(F.max(gcam) - F.min(gcam)).data
